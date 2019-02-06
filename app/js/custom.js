4e220549713d0d9e59af83c5047db60dc8e208eb
Prathuish <gprathuis@gmail.com>
2019-02-06 23:14:02 +0530

    Drop down script added

--- app/js/custom.js
+++ app/js/custom.js
@@ -41,3 +41,56 @@ function drawChart() {
 
   chart.draw(workqueue_items_data2, workqueue_items_options2);
 }
+
+// sample data set
+var users = [
+  {id: 1, name: 'Mark Miller'},
+  {id: 2, name: 'Bill Wilson'},
+  {id: 3, name: 'John Moore'},
+  {id: 4, name: 'Alfred Taylor'}
+];
+
+// set the drop donw the drop down list
+function setDropdDownList(keyword){
+  // $("#search-results").addClass('show');
+  $("#search-results").empty();
+  var options = users.filter(function(user){
+    if(keyword){
+      return user.name.toLowerCase().search(keyword.toLowerCase()) != -1
+    }else{
+      return true;
+    }
+    
+  }).map(function(user) {
+    return '<a class="dropdown-item" href="#" data-user-id="'+user.id+'" onclick="selectUser($event)">'+user.name+'</a>'
+  });
+
+  $("#search-results").append(options);
+};
+// set drop down list on page load
+setDropdDownList();
+
+// on user select
+function selectUser(user) {
+  console.log(user)
+}
+
+
+// on search value change 
+$("#search-input").bind("keyup", function(e) {
+  console.log(e.target.value);
+  setDropdDownList(e.target.value);
+
+})
+
+// on search focus display the drop down list
+$( "#search-input" ).focus(function() {
+  $("#search-results").addClass('show');
+});
+
+// on search focus out display the drop down list
+$( "#search-input" ).blur(function() {
+  $("#search-results").removeClass('show');
+});
+
+
